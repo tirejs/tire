22350e062c2517947923f633170ca15c282e9818
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2013-02-04 19:13:06 +0100

    jsonp and ajax module test should also be turned of via ?ajax=false

--- test/tests/core-tests.js
+++ test/tests/core-tests.js
@@ -366,33 +366,33 @@ test('Should be able to unbind specific events using', function () {
   $('.html').on('click', a).on('click', b).off('click', b).trigger('click');
 });
 
-module('Tire xhr.js', {
-  setup: function () {
-    var srh = XMLHttpRequest.prototype.setRequestHeader;
-
-    window.headers = {};
-
-    XMLHttpRequest.prototype.setRequestHeader = function (key, val) {
-        window.headers[key] = val;
-        srh.call(this, key, val);
-    }
-  }
-});
-
-test('get jsonp', function () {
-  stop();
-  $.ajax('http://echojson.com/hello/world?callback=?&history=false', function (data) {
-    start();
-    ok(data instanceof Object, true, 'Should return true if data is a instance of object');
-  });
-  stop();
-  $.ajax({ url: 'http://echojson.com/hello/world?callback=?&history=false', dataType: 'jsonp' }, function (data) {
-    start();
-    ok(data instanceof Object, true, 'Should return true if data is a instance of object');
-  });
-});
-
 if (window.location.protocol.indexOf('http') !== -1 && window.location.search.indexOf('ajax=false') === -1) {
+  module('Tire xhr.js', {
+    setup: function () {
+      var srh = XMLHttpRequest.prototype.setRequestHeader;
+
+      window.headers = {};
+
+      XMLHttpRequest.prototype.setRequestHeader = function (key, val) {
+          window.headers[key] = val;
+          srh.call(this, key, val);
+      }
+    }
+  });
+  
+  test('get jsonp', function () {
+    stop();
+    $.ajax('http://echojson.com/hello/world?callback=?&history=false', function (data) {
+      start();
+      ok(data instanceof Object, true, 'Should return true if data is a instance of object');
+    });
+    stop();
+    $.ajax({ url: 'http://echojson.com/hello/world?callback=?&history=false', dataType: 'jsonp' }, function (data) {
+      start();
+      ok(data instanceof Object, true, 'Should return true if data is a instance of object');
+    });
+  });
+
   test('get json', function () {
     stop();
     $.ajax('test.json', function (data) {
