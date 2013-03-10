8fa56454d8ef397904926f4241722b96d7aeb867
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2013-03-10 22:50:45 +0100

    Removing AMD module. Will be moved to custom builds later.

--- src/footer.js
+++ src/footer.js
@@ -1,11 +1,5 @@
 
   // Expose tire to the global object
   window.$ = window.tire = tire;
-
-  // Expose tire as amd module
-  if (typeof define === 'function' && define.amd) {
-    define('tire', [], function () {
-      return tire;
-    });
-  }
+  
 }(window));
