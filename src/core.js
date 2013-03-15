2a5b5e8102d708b650b6240c0d42e080982b49f9
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2013-03-15 15:46:22 +0100

    Fixing selectors

--- src/core.js
+++ src/core.js
@@ -1,8 +1,8 @@
 var document   = window.document
   , _tire      = window.tire
   , _$         = window.$
-  , idExp      = /^#[\w\-]+$/
-  , classExp   = /^\.[\w\-]+$/
+  , idExp      = /^#([\w\-]*)$/
+  , classExp   = /^\.([\w\-]*)$/
   , tagNameExp = /^[\w\-]+$/
   , tagExp     = /<([\w:]+)/
   , slice      = [].slice;
